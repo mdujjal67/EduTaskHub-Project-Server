de1504a848bce4993978687715a78f1fcfcfc476
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-12 21:26:53 +0600

    mongoDB setup with .env file

--- index.js
+++ index.js
@@ -6,8 +6,55 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
+
+// --------------------------MongoDB Start-------------------------------
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7tyfnet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const featuresCollection = client.db('EduTaskHub').collection('features');
+
+    
+    // ----------------services related api -------------------
+    // get all features data
+    app.get('/features', async (req, res) => {
+        const cursor = featuresCollection.find();   //In one line: const result = await featuresCollection.find().toArray()
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+
+    
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// -----------------------------MongoDB End-----------------------------
+
 
 
 
